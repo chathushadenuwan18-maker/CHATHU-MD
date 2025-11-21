2c768e076aa68383bacbe0e9003ae258a8ed9182
chathushadenuwan18-maker <chathushadenuwan18@gmail.com>
2025-11-21 22:41:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/chathushadenuwan18-maker/CHATHU-MD/blob/main/images/6127343109646519255.jpg?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 CHATHU-MD Is Alive Now😍*",
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
 
